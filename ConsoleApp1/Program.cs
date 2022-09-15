6d52c092edecd6ee6771f92f1130b44c0879012a
KHLS29 <KHLS29@I417-09>
2022-09-15 09:09:54 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Thrash> trashList = new List<Thrash>();
+            bool loop = false;
+            while(!loop)
+            {
+                try
+                {
+                    trashList.Add(new Thrash());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    loop = true;
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Thrash.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Thrash
+    {
+        static int szamlalo = 0;
+        public Thrash()
+        {
+            szamlalo++;
+            Console.WriteLine("Object letrehozva, Count{0}", szamlalo);
+        }
+        ~Thrash()
+        {
+            szamlalo--;
+            Console.WriteLine("Object törölve, Count{0}", szamlalo);
+        }
+    }
+}
